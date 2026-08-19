20fb54c2d025627d31cd4a0d374f0840557725f3
gecid-004 <kevin.borges.700@ufrn.edu.br>
2026-08-19 11:51:58 -0300

    feat: restyle PCIRN search page

--- src/themes/custom/app/search-page/search-page.component.ts
+++ src/themes/custom/app/search-page/search-page.component.ts
@@ -7,9 +7,8 @@ import { ThemedSearchComponent } from '../../../../app/shared/search/themed-sear
 
 @Component({
   selector: 'ds-themed-search-page',
-  // styleUrls: ['./search-page.component.scss'],
-  // templateUrl: './search-page.component.html'
-  templateUrl: '../../../../app/search-page/search-page.component.html',
+  templateUrl: './search-page.component.html',
+  styleUrls: ['./search-page.component.scss'],
   providers: [
     {
       provide: SEARCH_CONFIG_SERVICE,
